5192d7ba832b504a3fe892d4efd36b3698b7e5bf
Mike McPherson <mmcp42@yahoo.com>
2013-12-31 16:15:53 +0000

    Mike's data record definition
    based on sodaq variant

--- DataRecord.h
+++ DataRecord.h
@@ -3,22 +3,27 @@
 
 #include <Sodaq_dataflash.h>
 
-#define HEADER_MAGIC            "SODAQ"
-#define DATA_VERSION            2               // Please register at http://sodaq.net/
+#define HEADER_MAGIC            "wxftp"
+#define DATA_VERSION            3               // Please register at http://sodaq.net/
 struct myHeader
 {
-  uint32_t      ts;
-  uint32_t      version;
-  char          magic[6];
+  uint32_t      ts;			// timestamp
+  uint16_t      page;		// flash page number
+  uint32_t      version;	// data version
+  char          magic[6];	// magic string
 };
 typedef struct myHeader myHeader;
 
 struct myRecord
 {
-  uint32_t      ts;             // seconds since epoch (01-jan-1970)
+  // seconds since epoch (01-jan-1970)
+  uint32_t      ts;             
 
-  //raincounters
-  uint8_t       rain_ticks;
+  // flash page number
+  uint16_t      page;
+
+  // raincounter
+  uint16_t      rain_ticks;
 
   // wind
   uint16_t      wind_ticks;
@@ -28,16 +33,17 @@ struct myRecord
   uint16_t      wind_gust_dir;
   uint16_t      wind_lull_dir;
 
-  //battery
+  // battery
   uint16_t      batteryVoltage;
+  uint16_t      temperatureRTC;
   
-  //humidity
-  uint16_t		temperatureH;
-  uint16_t		humidity;
+  // humidity
+  uint16_t      humidity;
+  uint16_t      temperatureH;
   
-  //pressure
-  float			temperature;
-  float			pressure;
+  // pressure
+  uint16_t      pressure;
+  uint16_t      temperatureP;
 };
 typedef struct myRecord myRecord;
 //extern myRecord rec;
